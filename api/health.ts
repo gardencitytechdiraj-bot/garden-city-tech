3f51b6d2357dd47a3c4c16d4369dcc92d2b998cd
wetech2026 <gardencitytech.diraj@gmail.com>
2026-08-12 13:20:57 +0545

    Update health.tsFix duplicated health route

--- api/health.ts
+++ api/health.ts
@@ -3,9 +3,4 @@ import { handleHealthRequest } from "../server/application-handler.js";
 
 export default function health(request: IncomingMessage, response: ServerResponse) {
   return handleHealthRequest(request, response);
-}import type { IncomingMessage, ServerResponse } from "node:http";
-import { handleHealthRequest } from "../server/application-handler";
-
-export default function health(request: IncomingMessage, response: ServerResponse) {
-  return handleHealthRequest(request, response);
 }
