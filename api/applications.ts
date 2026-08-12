8ca788d84cd98bb5751a6e632142cd681786d6c2
wetech2026 <gardencitytech.diraj@gmail.com>
2026-08-12 12:59:23 +0545

    Fix Vercel ESM API handler import

--- api/applications.ts
+++ api/applications.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { handleApplicationsRequest } from "../server/application-handler";
+import { handleApplicationsRequest } from "../server/application-handler.js";
 
 export default function applications(request: IncomingMessage, response: ServerResponse) {
   return handleApplicationsRequest(request, response);
